269581066f32b1a0005217a28391a41512aea046
vipulpisal <65292102+vipulpisal@users.noreply.github.com>
2025-02-21 18:30:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🎈 Nifty Range Prediction ')
 
 st.write('this data is based on ML prediction')
+
+df = pd.read_csv('https://raw.githubusercontent.com/vipulpisal/csv-files/refs/heads/main/NIFTY%2050_Historical_PR_01012025to21022025.csv')
+df
